17b422cb765a9dac67f48f7f77a5405564e27fc9
and96437345 <and96437345@yandex.ru>
2024-03-05 22:30:29 +0300

    dz_16
    php_func

---
+++ func.php
@@ -0,0 +1,17 @@
+<?php
+
+function sub($a, $b) {
+    return $a - $b;
+}
+
+function mul($a, $b) {
+    return $a * $b;
+}
+
+function div($a, $b) {
+    return $a / $b;
+}
+
+function rem($a, $b) {
+    return $a % $b;
+}
